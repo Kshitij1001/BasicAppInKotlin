135bd158aa701f1c522908a63b43bb84c252e463
Kshitij1001 <kshitij4078@gmail.com>
2020-02-23 03:22:28 +0530

    navigate to second activity by a push button

--- app/src/main/java/com/kschitiz/basicappinkotlin/MainActivity.kt
+++ app/src/main/java/com/kschitiz/basicappinkotlin/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.kschitiz.basicappinkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnToast.setOnClickListener {
             Toast.makeText(this,"Button pressed",Toast.LENGTH_SHORT).show()
+        }
+        var yo = ""
+        btnNxtActivity.setOnClickListener {
+            yo = enterMsg.text.toString()
+            Toast.makeText(this,"You entered $yo",Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,SecondActivity::class.java))
         }
     }
 }
